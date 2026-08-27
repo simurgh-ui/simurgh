f84b6fae91a45903332f0d82f4776d0a70e42dc8
alighafoorzade <alighafoorzade@gmail.com>
2026-08-28 01:03:19 +0330

    feat(docs): add Google Tag Manager

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -16,6 +16,14 @@ export default defineConfig({
       head: [
         {
           tag: 'script',
+          content: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-ML86N84W');`,
+        },
+        {
+          tag: 'script',
           attrs: {
             async: true,
             src: 'https://www.googletagmanager.com/gtag/js?id=G-J6TJG3BEDL',
